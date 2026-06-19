308999a43f30d2a927e65b80ccefc2c2bc61d7e6
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-19 22:30:07 +0530

    Integrated OpenWeather API

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+API_KEY = "74068c6e6aaad08e5bfc563b94f7910e"
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    weather = None
+
+    if request.method == "POST":
+
+        city = request.form["city"]
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+        response = requests.get(url)
+
+        data = response.json()
+        print(data)
+        if data.get("cod") == 200:
+
+            weather = {
+                "city": data["name"],
+                "temp": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "wind": data["wind"]["speed"],
+                "condition": data["weather"][0]["description"]
+            }
+
+    return render_template(
+        "index.html",
+        weather=weather
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
