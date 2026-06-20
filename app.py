67d6d7ee4461d3d21bb765eb2b12072f1e4b7018
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-20 20:37:10 +0530

    Added recent search history feature

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 API_KEY = "27bf7c1eabbec193a265e70d5b80f468"
 
+search_history = []
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+
+    global search_history
 
     weather = None
     forecast = []
@@ -15,6 +19,13 @@ def home():
     if request.method == "POST":
 
         city = request.form["city"]
+        print("City Search:", city)
+
+        if city and city not in search_history:
+            search_history.append(city)
+
+        if len(search_history) > 5:
+            search_history.pop(0)
 
         weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
@@ -24,9 +35,10 @@ def home():
         forecast_response = requests.get(forecast_url)
 
         data = weather_response.json()
+        print(data)
         forecast_data = forecast_response.json()
 
-        if data.get("cod") == 200:
+        if str(data.get("cod")) == "200":
 
             weather = {
                 "city": data["name"],
@@ -37,13 +49,15 @@ def home():
                 "icon": data["weather"][0]["icon"]
             }
 
-            for item in forecast_data["list"][:5]:
+            if "list" in forecast_data:
 
-                forecast.append({
-                    "temp": item["main"]["temp"],
-                    "icon": item["weather"][0]["icon"],
-                    "date": item["dt_txt"]
-                })
+                for item in forecast_data["list"][:5]:
+
+                    forecast.append({
+                        "temp": item["main"]["temp"],
+                        "icon": item["weather"][0]["icon"],
+                        "date": item["dt_txt"]
+                    })
 
         else:
             error = "City not found or API issue."
@@ -52,7 +66,8 @@ def home():
         "index.html",
         weather=weather,
         forecast=forecast,
-        error=error
+        error=error,
+        history=search_history
     )
 
 if __name__ == "__main__":
