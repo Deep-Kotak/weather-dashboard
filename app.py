0b63cef841a9b1e1bcf20c07a0e20bb9c4814de8
Deep-Kotak <deepmkotak1103@gmail.com>
2026-06-20 20:14:20 +0530

    Added 5 day weather forecast feature

--- app.py
+++ app.py
@@ -3,25 +3,28 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = "74068c6e6aaad08e5bfc563b94f7910e"
+API_KEY = "27bf7c1eabbec193a265e70d5b80f468"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     weather = None
+    forecast = []
     error = None
 
     if request.method == "POST":
 
         city = request.form["city"]
 
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-        response = requests.get(url)
+        forecast_url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&units=metric"
 
-        data = response.json()
+        weather_response = requests.get(weather_url)
+        forecast_response = requests.get(forecast_url)
 
-        print(data)
+        data = weather_response.json()
+        forecast_data = forecast_response.json()
 
         if data.get("cod") == 200:
 
@@ -34,12 +37,21 @@ def home():
                 "icon": data["weather"][0]["icon"]
             }
 
+            for item in forecast_data["list"][:5]:
+
+                forecast.append({
+                    "temp": item["main"]["temp"],
+                    "icon": item["weather"][0]["icon"],
+                    "date": item["dt_txt"]
+                })
+
         else:
             error = "City not found or API issue."
 
     return render_template(
         "index.html",
         weather=weather,
+        forecast=forecast,
         error=error
     )
 
